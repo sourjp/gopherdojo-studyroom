794df8e3eaea5ee88d1823f40fcb2fa0822a8737
sourjp <hiro0209.ki@gmail.com>
2020-08-11 12:44:59 +0900

    first commit for kadai1

---
+++ kadai1/cmd/conv/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	conv "github.com/sourjp/gopherdojo-studyroom/kadai1"
+)
+
+var (
+	baseDir string
+	srcExt  string = "jpg"
+	dstExt  string = "png"
+)
+
+func init() {
+	flag.StringVar(&srcExt, "se", srcExt, "source exetention to decode(jpg, jpeg, png, gif)")
+	flag.StringVar(&dstExt, "de", dstExt, "destinate extension to encode(jpg, jpeg, png, gif)")
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Image convert was suceeded!")
+}
+
+func run() error {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 1 {
+		return errors.New("Please specifiy a directory, conv find image files recursively")
+	}
+	if _, err := os.Stat(args[0]); os.IsNotExist(err) {
+		return fmt.Errorf("Failed to found directory: dir=%s", args[0])
+	}
+
+	c := conv.New(args[0], srcExt, dstExt)
+	if ok := c.IsValidatedExt(); !ok {
+		return fmt.Errorf("Failed to read specified extention: srcExt=%s, dstExt=%s", srcExt, dstExt)
+	}
+
+	paths, err := c.GetImagePaths()
+	if err != nil {
+		return err
+	}
+
+	for _, path := range paths {
+		img, err := c.Decode(path)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		err = c.Encode(path, img)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+	}
+	return nil
+}
---
+++ kadai1/conv.go
@@ -0,0 +1,119 @@
+package conv
+
+import (
+	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+)
+
+// Converter defines structure of converting informations.
+type Converter struct {
+	BaseDir string
+	SrcExt  string
+	DstExt  string
+}
+
+// New construct and return Converter.
+func New(d, se, de string) *Converter {
+	return &Converter{BaseDir: d, SrcExt: se, DstExt: de}
+}
+
+// IsValidatedExt validate extentions.
+func (c *Converter) IsValidatedExt() bool {
+	var supportExt []string = []string{"jpg", "jpeg", "png", "gif"}
+	var src, dst bool
+	for _, v := range supportExt {
+		if c.SrcExt == v {
+			src = true
+		}
+		if c.DstExt == v {
+			dst = true
+		}
+	}
+	return src && dst
+}
+
+// GetImagePaths search specified extentionis image files recusively and returns them.
+func (c *Converter) GetImagePaths() ([]string, error) {
+	var paths []string
+	err := filepath.Walk(c.BaseDir, func(path string, info os.FileInfo, err error) error {
+		ext := "." + c.SrcExt
+		if filepath.Ext(path) == ext {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(paths) == 0 {
+		return nil, fmt.Errorf("Failed to find image files:  dir=%s, ext=.%s", c.SrcExt, c.BaseDir)
+	}
+	return paths, nil
+}
+
+// Decode returns ImageData which compose file path and decoded image data.
+func (c *Converter) Decode(p string) (img image.Image, err error) {
+	f, err := os.Open(p)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open image file: file=%s, err=%v", p, err)
+	}
+	defer func() {
+		if rerr := f.Close(); rerr != nil {
+			err = fmt.Errorf("Failed to close file: %v, the original error was %v", rerr, err)
+		}
+	}()
+
+	switch c.SrcExt {
+	case "jpg", "jpeg":
+		img, err = jpeg.Decode(f)
+	case "png":
+		img, err = png.Decode(f)
+	case "gif":
+		img, err = gif.Decode(f)
+	default:
+		return nil, fmt.Errorf("An extention doesn't support to decode: ext=%s", c.SrcExt)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode iamge file: file=%s, err=%v", p, err)
+	}
+
+	return img, nil
+}
+
+// Encode encodes image file.
+func (c *Converter) Encode(p string, img image.Image) (err error) {
+	fname := p[:len(p)-len(filepath.Ext(p))] + "." + c.DstExt
+	f, err := os.Create(fname)
+	if err != nil {
+		return fmt.Errorf("Failed to create image file: file=%s, err=%v", p, err)
+	}
+	defer func() {
+		if rerr := f.Close(); rerr != nil {
+			err = fmt.Errorf("Failed to close file: %v, the original error was %v", rerr, err)
+		}
+	}()
+
+	switch c.DstExt {
+	case "jpg", "jpeg":
+		err = jpeg.Encode(f, img, nil)
+	case "png":
+		err = png.Encode(f, img)
+	case "gif":
+		err = gif.Encode(f, img, nil)
+	default:
+		defer os.Remove(fname)
+		return fmt.Errorf("An extention doesn't support to encode: ext=%s", c.DstExt)
+	}
+	if err != nil {
+		defer os.Remove(fname)
+		return fmt.Errorf("Failed to encode iamge file: file=%s, err=%v", p, err)
+	}
+
+	return nil
+}
